fc4a609dd8857b20c07f112c12d8b18acef19ae4
Juancarlos Jauregui <jauregui3@gmail.com>
2017-05-18 16:28:59 -0700

    Implement addNode, contains, and removeNode in Graph interface

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,38 +2,48 @@
 
 // Instantiate a new graph
 var Graph = function() {
+  this.nodes = [];
 };
+
+
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this.nodes.push(node);
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return this.nodes.includes(node);
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  if (this.nodes.indexOf(node) !== -1) {
+    this.nodes.splice(this.nodes.indexOf(node), 1);
+  }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
