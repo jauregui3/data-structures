d89735614debdbaab50666a75321d34bfa721ad8
Juancarlos Jauregui <jauregui3@gmail.com>
2017-05-18 15:52:37 -0700

    Implement tree interface

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,9 +1,9 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
+  _.extend(newTree, treeMethods);
   // your code here
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -11,9 +11,25 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  var result = false;
+
+  var traverseTree = function(root) {
+    if (root.value === target) {
+      result = true;
+    } else {
+      for (var i = 0; i < root.children.length; i++) {
+        traverseTree(root.children[i]);
+      }
+    }
+  };
+
+  traverseTree(this);
+  return result;
 };
 
 
