3c0d6710fdfd98cc0190afaabe6553407c841c45
Juancarlos Jauregui <jauregui3@gmail.com>
2017-05-19 11:29:58 -0700

    Implement insert and remove functions for hashTable interface

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,26 +1,44 @@
-
-
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+
+  var tuple = function(key, value) {
+    var obj = [];
+    obj.push(key, value);
+    return obj;
+  }
 };
 
 HashTable.prototype.insert = function(k, v) {
+  // hash function is getIndexBelowMaxForKey
   var index = getIndexBelowMaxForKey(k, this._limit);
+  // insert key at index in storage
+  this._storage.set(index, tuple(k, v));
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var tupleCollection = this._storage.get(index);
+  for (var i = 0; i < tupleCollection.length; i++) {
+    if (k = tupleCollection[i][0]) {
+      return tupleCollection[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  this._storage.set(index, undefined);
 };
 
 
 
+
+
+// [1, , 3, , , 6, 7, 8]
+
+// set > eval to 6
+
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
