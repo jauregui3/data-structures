a71ba389b02b661a49ac798fcf9ae6013165e450
Juancarlos Jauregui <jauregui3@gmail.com>
2017-05-18 20:33:32 -0700

    Complete graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,47 +1,57 @@
-
-
 // Instantiate a new graph
 var Graph = function() {
-  this.nodes = [];
+  this.nodes = {};
 };
-
-
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
-  this.nodes.push(node);
+  this.nodes[node] = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
-  return this.nodes.includes(node);
+  return this.nodes.hasOwnProperty(node);
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
-  if (this.nodes.indexOf(node) !== -1) {
-    this.nodes.splice(this.nodes.indexOf(node), 1);
+  for (var i = 0; 1 < this.nodes[node].length; i++) {
+    var temp = this.nodes[node][i];
+    removeEdge(this.nodes[temp], this.nodes[node]);
+  }
+
+  if (this.nodes[node]) {
+    delete this.nodes[node];
   }
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
-
+  if (this.nodes[fromNode] && this.nodes[toNode]) {
+    return this.nodes[fromNode].includes(toNode);
+  }
+  return false;
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
-
+  if (this.nodes[fromNode] && this.nodes[toNode]) {
+    this.nodes[fromNode].push(toNode);
+    this.nodes[toNode].push(fromNode);
+  }
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
-
+  this.nodes[fromNode].splice(this.nodes[fromNode].indexOf(toNode), 1);
+  this.nodes[toNode].splice(this.nodes[toNode].indexOf(fromNode), 1);
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
-
+  for (var prop in this.nodes) {
+    cb(prop);
+  }
 };
 
 /*
