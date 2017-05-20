d2c289ca30fd123443508b4757601757caa09b66
Juancarlos Jauregui <jauregui3@gmail.com>
2017-05-20 11:09:11 -0700

    Add test that checks if edges were added in both directions

--- sprint-two/spec/graphSpec.js
+++ sprint-two/spec/graphSpec.js
@@ -36,6 +36,16 @@ describe('graph', function() {
     expect(graph.hasEdge(3, 1)).to.equal(false);
   });
 
+  // since it's an implementation for undirected graphs
+  it('should create edges in both directions', function() {
+    graph.addNode(2);
+    graph.addNode(1);
+    graph.addNode(3);
+    graph.addEdge(3, 2);
+    expect(graph.hasEdge(3, 2)).to.equal(true);
+    expect(graph.hasEdge(2, 3)).to.equal(true);
+  })
+
   it('should remove edges between nodes', function() {
     graph.addNode(4);
     graph.addNode(5);
